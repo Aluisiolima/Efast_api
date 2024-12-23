25f844ab3f9b679adeda74a29aa5b261b7582498
aluisio lima <aaluisio309@gmail.com>
2024-12-23 11:36:05 -0300

    feat: ProdutosModel => documentado seus metodos

--- src/Model/ProdutosModel.php
+++ src/Model/ProdutosModel.php
@@ -1,10 +1,20 @@
 <?php
     namespace App\Model;
     use PDO;
-use PDOException;
+    use PDOException;
 
+    /**
+     * Class ProdutosModel 
+     * Esta Class é responsável pelas interacoes no banco na table Produtos
+     */
     class ProdutosModel extends Database
     {
+        /**
+         * Pegar os registro de produtos que tem seu status Ativo
+         * @param array $data contendo as chaves 
+         *  - "id_empresa" : INT sendo o id corresponde da empresa que voce que os produtos  
+         * @return array
+         */
         public static function pegarProdutos(array $data): array
         {
             $pdo = self::getConnection();
@@ -19,6 +29,17 @@ use PDOException;
             return $produtos;
            
         }
+
+        /**
+         * Inserir os dados de novo produto
+         * @param array $data contendo as chaves 
+         *  - "nome" : STRIGN sendo o nome do produto 
+         *  - "valor" : INT sendo o seu valor de mercado
+         *  - "tipo" : STRIGN sendo sua tipagem
+         *  - "id_img" : INT sendo o id do seu arquivo de img para layout
+         *  - "id_empresa" : INT sendo o id da empressa qual o produto e relacionado 
+         * @return array
+         */
         public static function inseriProdutos(array $data): array
         {
             try {
@@ -43,6 +64,16 @@ use PDOException;
            
         }
         
+        /**
+         * Editar os dados dos produto
+         * @param array $data contendo as chaves 
+         *  - "nome" : STRIGN sendo o nome do produto 
+         *  - "valor" : INT sendo o seu valor de mercado
+         *  - "tipo" : STRIGN sendo sua tipagem
+         *  - "id_img" : INT sendo o id do seu arquivo de img para layout
+         *  - "id" : INT sendo o id do produto qual o voce que edit 
+         * @return array
+         */
         public static function updateProdutos(array $data): array
         {
             try {
@@ -72,6 +103,12 @@ use PDOException;
             }
         }
 
+        /**
+         * Desativa o produto para nao ser comecializado
+         * @param array $data contendo as chaves 
+         *  - "id" : INT sendo o id do produto 
+         * @return array
+         */
         public static function desativaProdutos(array $data): array
         {
             try {
@@ -90,6 +127,13 @@ use PDOException;
                 return ["error"=> $e->getMessage()];
             }
         }
+
+        /**
+         * Ativa o produto para ser comecializado
+         * @param array $data contendo as chaves 
+         *  - "id" : INT sendo o id do produto 
+         * @return array
+         */
         public static function ativaProdutos(array $data): array
         {
             try {
