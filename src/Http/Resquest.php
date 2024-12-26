3e3297e39f54f7b831b5784d0524f2fe539381ff
Aluisiolima <aaluisio309@gmail.com>
2024-12-26 15:57:09 -0300

    feat: Resquest => criado o metodo de authorization para pegar o token passado ela requisicao

--- src/Http/Resquest.php
+++ src/Http/Resquest.php
@@ -26,14 +26,32 @@
          */
         public static function getBody(): mixed
         {
-            $json = json_decode(file_get_contents('php://input'), true) ?? [];
+            $json = json_decode(file_get_contents("php://input"), true) ?? [];
 
             $data = match (self::method()) {
-                'GET' => $_GET,
-                'POST' => !empty($_FILES) ? array_merge($json, $_FILES) : $json,
-                'PUT', 'DELETE' => $json,
+                "GET" => $_GET,
+                "POST" => !empty($_FILES) ? array_merge($json, $_FILES) : $json,
+                "PUT", "DELETE" => $json,
             };
 
             return $data;
+        }
+
+        /**
+         * Retorna o token passado pela requisicao ou um array de error caso nao passase o token
+         *
+         * @return array|string
+         */
+        public static function authorization(): array|string
+        {
+            $authorization = getallheaders();
+
+            if (!isset($authorization["Authorization"])) return ["error" => "Desculpada mais voce nao passou o Token"];
+
+            $authorizationPartials = explode(" ", $authorization["Authorization"]);
+
+            if (count($authorizationPartials) != 2) return ["error"=> "O token foi passado errado confira-lo..."];
+
+            return $authorizationPartials[1] ?? "";
         }
     }
