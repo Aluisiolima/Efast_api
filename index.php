2c0124f40b3bec7a5db24267e02ab239cf548d2a
Aluisio Lima <aaluisio309@gmail.com>
2025-01-30 09:03:12 -0300

    fix(bug): nao estava carregando as variaveis de ambiente na hospedagem pois la nao e docker

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
     require_once __DIR__ ."/vendor/autoload.php";
     require_once __DIR__ ."/src/Routes/main.php";
     require_once __DIR__ ."/config.php";
-
-    // codigo a ser usado se nao tiver o docker 
     
-    // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    // $dotenv->load();
-
+    if (class_exists("Dotenv\Dotenv")) {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+    }
     use App\Core\Core;
     use App\Http\Routes;
 
