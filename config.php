e31bf63735f1311a57a3d8dea4607c9fe7c5f57c
Aluisio Lima <aaluisio309@gmail.com>
2025-04-20 09:56:39 -0300

    teste 2 de cors

--- config.php
+++ config.php
@@ -1,10 +1,18 @@
 <?php
     declare(strict_types=1);
-    header("Content-Type: application/json");
-    header("Access-Control-Allow-Origin: [https://efastmenu.com, https://dev.efastmenu.com]");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
+    $allowed_origins = [
+        'https://efastmenu.com',
+        'https://dev.efastmenu.com.com',
+    ];
+    
+   
+    if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+        header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        header("Access-Control-Allow-Credentials: true");
+    }
+    
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200);
         exit(); 
