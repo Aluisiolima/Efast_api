da462896f3c1cb7812f1b48072557fa266afc158
Aluisio Lima <aaluisio309@gmail.com>
2025-04-20 10:04:33 -0300

    fix(): cors

--- config.php
+++ config.php
@@ -1,17 +1,11 @@
 <?php
     declare(strict_types=1);
-    $allowed_origins = [
-        'https://efastmenu.com',
-        'https://dev.efastmenu.com.com',
-    ];
+  
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Credentials: true");
     
-   
-    if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
-        header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-        header("Access-Control-Allow-Headers: Content-Type, Authorization");
-        header("Access-Control-Allow-Credentials: true");
-    }
     
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         http_response_code(200);
