51dc6a2a1b90c9a41251cfcf0d5de180722d4e1a
Aluisio Lima <aaluisio309@gmail.com>
2025-05-20 17:40:59 -0300

    feat(Token): troca de 5 min para 1 hora de expiracao do token

--- src/Http/JWToken.php
+++ src/Http/JWToken.php
@@ -31,7 +31,7 @@
             try {
                 // Obtém a chave secreta do ambiente
                 self::$secret = $_ENV["SECRET_KEY"];
-                $data["exp"] = time() + 300; // Define a expiração do token para 1 hora
+                $data["exp"] = time() + (60 * 60); // Define a expiração do token para 1 hora
                 $data["iat"] = time(); // Define a data de emissão do token
 
                 // Codifica os dados no token JWT
