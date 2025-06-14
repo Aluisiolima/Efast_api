761261d97ec8b5c164bb5f19f0792144805a880a
Aluisio Lima <aaluisio309@gmail.com>
2025-06-13 21:08:23 -0300

    feat(): add de observabilidade na api

--- src/Core/Core.php
+++ src/Core/Core.php
@@ -1,38 +1,40 @@
 <?php
-namespace App\Core;
+    namespace App\Core;
 
-use App\Http\Resquest;
-use App\Http\Response;
-use App\Core\ControllerResolver;
+    use App\Http\Resquest;
+    use App\Http\Response;
+    use App\Core\ControllerResolver;
+    use App\Utils\Logs;
 
-class Core
-{
-    public static function dispatch(array $routes): void
+    class Core
     {
-        $url = "/" . ($_GET["url"] ?? '');
-        $url != "/" && $url = rtrim($url, "/");
+        public static function dispatch(array $routes): void
+        {
+            $url = "/" . ($_GET["url"] ?? '');
+            $url != "/" && $url = rtrim($url, "/");
 
-        foreach ($routes as $route) {
-            $pattern = "#^" . preg_replace("/{id}/", "([\w-]+)", $route["path"]) . "$#";
+            foreach ($routes as $route) {
+                $pattern = "#^" . preg_replace("/{id}/", "([\w-]+)", $route["path"]) . "$#";
 
-            if (preg_match($pattern, $url, $matches)) {
-                array_shift($matches);
+                if (preg_match($pattern, $url, $matches)) {
+                    array_shift($matches);
 
-                if ($route["method"] !== Resquest::method()) {
-                    Response::json(["error" => "Método inválido"], 405);
+                    if ($route["method"] !== Resquest::method()) {
+                        Response::json(["error" => "Método inválido"], 405);
+                        return;
+                    }
+
+                    [$controllerName, $action] = explode("@", $route["action"]);
+                
+                    $controller = ControllerResolver::criar($controllerName);
+                    Logs::log_request();
+                    $controller->$action(...$matches);
+                    
                     return;
                 }
-
-                [$controllerName, $action] = explode("@", $route["action"]);
-            
-                $controller = ControllerResolver::criar($controllerName);
-                $controller->$action(...$matches);
-                
-                return;
             }
-        }
 
-        $controller = ControllerResolver::criar("NotFoundController");
-        $controller->index();
+            $controller = ControllerResolver::criar("NotFoundController");
+            $controller->index();
+        }
     }
-}
