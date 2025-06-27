6ae8d6ade8345acfd4b1201333f0f8bf1406efe4
Aluisio Lima <aaluisio309@gmail.com>
2025-06-27 20:28:38 -0300

    test(): velocidade de frete

--- src/Services/FreteServices.php
+++ src/Services/FreteServices.php
@@ -47,26 +47,18 @@
         }
 
         private function calcularDistancia($lat1, $lon1, $lat2, $lon2): int {
-            $raioTerra = 6371; // Raio da Terra em km
-        
-            // Converte graus para radianos
-            $lat1 = deg2rad($lat1);
-            $lon1 = deg2rad($lon1);
-            $lat2 = deg2rad($lat2);
-            $lon2 = deg2rad($lon2);
-        
-            // Diferenças
-            $dLat = $lat2 - $lat1;
-            $dLon = $lon2 - $lon1;
-        
-            // Fórmula de Haversine
-            $a = sin($dLat/2) * sin($dLat/2) +
-                 cos($lat1) * cos($lat2) *
-                 sin($dLon/2) * sin($dLon/2);
-            $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
-        
-            $distancia = $raioTerra * $c;
-            return $distancia ;
+            static $raioTerra = 6371; // km
+
+            $lat1Rad = deg2rad($lat1);
+            $lat2Rad = deg2rad($lat2);
+            $dLat = deg2rad($lat2 - $lat1);
+            $dLon = deg2rad($lon2 - $lon1);
+
+            $a = sin($dLat / 2) ** 2 +
+                cos($lat1Rad) * cos($lat2Rad) *
+                sin($dLon / 2) ** 2;
+
+            return $raioTerra * (2 * atan2(sqrt($a), sqrt(1 - $a)));
         }
 
     }
